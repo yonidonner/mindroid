8e4996e5831c2649370836f3e9130899c0653d13
Yoni Donner <yonidonner@gmail.com>
2012-05-22 10:00:17 -0700

    Some initial games

--- example/GoNoGo/src/com/example/gonogo/GoNoGoActivity.java
+++ example/GoNoGo/src/com/example/gonogo/GoNoGoActivity.java
@@ -1,7 +1,9 @@
 package com.example.gonogo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class GoNoGoActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +11,10 @@ public class GoNoGoActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    
+    public void startClicked(View view) {
+    	Intent intent = new Intent(this, GoNoGoTestActivity.class);
+        startActivity(intent);
     }
 }
